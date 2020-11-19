fbac3aff483d073d61d1918cd62b56f384ab060b
Grayson James <59737925+vidrinenatwit@users.noreply.github.com>
2020-11-19 14:41:13 -0500

    Created a separate class for each expression and utilizes recurssion and the Random class to create a random mathematical equation

--- src/edu/wit/cs/comp2000/RandomExpression.java
+++ src/edu/wit/cs/comp2000/RandomExpression.java
@@ -1,5 +1,7 @@
 package edu.wit.cs.comp2000;
 
+import java.util.Random;
+
 /**
  * A class that creates a grammar tree for a mathematical expression by
  * making random function choices.
@@ -10,13 +12,12 @@ public class RandomExpression extends Expression {
 	Expression root;
 	
 	public RandomExpression() {
-		// TODO: Implement method
+		this.root = buildRandTree(10);
 	}
 	
 	@Override
 	public double evaluate(double x, double y) {
-		// TODO: Implement method
-		return Double.NEGATIVE_INFINITY;
+		return root.evaluate(x, y);
 	}
 	
 
@@ -27,8 +28,23 @@ public class RandomExpression extends Expression {
 	 * @return the root of the resulting expression 
 	 */
 	private Expression buildRandTree(int maxDepth) {
-		// TODO: Implement method
-		return null;
+		Expression rValue = new terminalx();
+		if(maxDepth > 0)
+		{
+			--maxDepth;
+			Random expression = new Random();
+			Double nextDouble = expression.nextDouble();
+			if(nextDouble<.3) rValue = new Sine(buildRandTree(maxDepth));
+			else if (nextDouble>=.3 && nextDouble<.6) rValue = new Cosine(buildRandTree(maxDepth));
+			else if (nextDouble>=.6 && nextDouble<.725) rValue = new Average(buildRandTree(maxDepth), buildRandTree(maxDepth));
+			else if (nextDouble>=.725 && nextDouble<.85) rValue = new Multiply(buildRandTree(maxDepth), buildRandTree(maxDepth));
+			else {
+				Random xORy = new Random();
+				if(xORy.nextDouble() > 50) rValue = new terminalx();
+				else rValue = new terminaly();
+			}
+		}
+		return rValue;
 	}
 	
 	@Override
